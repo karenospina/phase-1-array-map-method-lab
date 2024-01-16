7c518d93e2d55bb62ac6fb3aa17c5d7e250f41bd
+ <+>
2024-01-15 21:52:26 -0500

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const tutorials = ['what does the this keyword mean?',
+  'What is the Constructor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'];
+
+  const titleCasedTutorials = tutorials.map(function(tutorial) {
+    return tutorial.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+  });
+
+  return titleCasedTutorials;
 }
+
+console.log(titleCased());
